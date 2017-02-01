cba9bf76124ba6f08bc49526e417f7d6001f6105
loglogin2 <i9991559@gmail.com>
2017-02-01 21:53:58 +0300

    if fixed

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -44,30 +44,20 @@ class TicTacToe {
 
     getWinner() {
 
-        if( this.playingField[0][0] == this.playingField[0][1] && this.playingField[0][0] == this.playingField[0][2] && this.playingField[0][0] != null)
+        for( var i = 0 ; i < 3 ; i ++ )
         {
-            return this.getFieldValue(0,0);
-        }
-        if( this.playingField[1][0] == this.playingField[1][1] && this.playingField[1][0] == this.playingField[1][2] && this.playingField[1][0] != null)
-        {
-            return this.getFieldValue(1,0);
-        }
-        if( this.playingField[2][0] == this.playingField[2][1] && this.playingField[2][0] == this.playingField[2][2] && this.playingField[2][0] != null)
-        {
-            return this.getFieldValue(2,0);
+                if (this.playingField[i][0] == this.playingField[i][1] && this.playingField[i][0] == this.playingField[i][2] && this.playingField[i][0] != null)
+                {
+                    return this.getFieldValue(i, 0);
+                }
         }
 
-        if( this.playingField[0][0] == this.playingField[1][0] && this.playingField[0][0] == this.playingField[2][0] && this.playingField[0][0] != null)
+        for( var i = 0 ; i < 3 ; i ++ )
         {
-            return this.getFieldValue(0,0);
-        }
-        if( this.playingField[0][1] == this.playingField[1][1] && this.playingField[0][1] == this.playingField[2][1] && this.playingField[0][1] != null)
-        {
-            return this.getFieldValue(0,1);
-        }
-        if( this.playingField[0][2] == this.playingField[1][2] && this.playingField[0][2] == this.playingField[2][2] && this.playingField[0][2] != null)
-        {
-            return this.getFieldValue(0,2);
+            if( this.playingField[0][i] == this.playingField[1][i] && this.playingField[0][i] == this.playingField[2][i] && this.playingField[0][i] != null)
+            {
+                return this.getFieldValue(0, i);
+            }
         }
 
         if( this.playingField[0][0] == this.playingField[1][1] && this.playingField[0][0] == this.playingField[2][2] && this.playingField[0][0] != null)
